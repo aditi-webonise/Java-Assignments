aa7e9e30a488012b8433cc1c601719ba5204084f
Aditi Mantri <aditi.mantri@webonise.com>
2017-12-12 13:26:20 +0530

    Add assignment5 - perform CRUD operations using JSP and Servlet

---
+++ Assignment5/src/main/java/controller/EditEmployeeController.java
@@ -0,0 +1,46 @@
+package main.java.controller;
+
+import main.java.service.ConnectionFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+
+public class EditEmployeeController extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request,HttpServletResponse response)throws ServletException,IOException{
+
+        try {
+            String redirect;
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+            Connection connection =connectionFactory.getConnection();
+
+            int id = Integer.parseInt(request.getParameter("id"));
+            String name = request.getParameter("name");
+            int contact = Integer.parseInt(request.getParameter("contact"));
+            String email = request.getParameter("email");
+            String bloodGroup = request.getParameter("bloodGroup");
+
+            String save = "UPDATE employees SET name= ?, contact = ?, email = ?, blood_group = ? where id = ?";
+            PreparedStatement statement = connection.prepareStatement(save);
+
+            statement.setString(1, name);
+            statement.setInt(2, contact);
+            statement.setString(3, email);
+            statement.setString(4, bloodGroup);
+            statement.setInt(5,id);
+            statement.executeUpdate();
+
+            redirect ="display.jsp";
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher(redirect);
+            requestDispatcher.forward(request,response);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Assignment5/web/error.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: webonise
+  Date: 7/12/17
+  Time: 5:29 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Error Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h3>Invalid Username or Password</h3>
+    <h4><a href="index.jsp">LOGIN</a></h4>
+</body>
+</html>
